c8b84383f51e1e4e6cda1b0cafdbb5ecf40b4357
Hakan Yildiz <107430060+Hakkology@users.noreply.github.com>
2022-09-13 23:24:37 +0300

    Add files via upload

---
+++ Hakan_ConsoleApp_EntityFrameWork/Program.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hakan_ConsoleApp_EntityFrameWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NORTHWND db = new NORTHWND();
+            var cat = db.Categories;
+            foreach (var item in cat)
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
